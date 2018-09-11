fd1b10c27da60642b5db5dd1d14788b195ecaa79
缪茂威 <morrisware01@gmail.com>
2018-09-11 14:08:29 +0800

    修改toast

--- library/src/main/java/com/morrisware/android/basic/ktx/ToastCompat.kt
+++ library/src/main/java/com/morrisware/android/basic/ktx/ToastCompat.kt
@@ -7,12 +7,18 @@ import android.widget.Toast
  * Created by MorrisWare on 2018/8/29.
  * Email: MorrisWare01@gmail.com
  */
+object ToastContainer {
+    var toast: Toast? = null
+}
+
 fun Context.toast(
     text: CharSequence,
     duration: Int = Toast.LENGTH_SHORT
 ): android.widget.Toast {
+    ToastContainer.toast?.cancel()
     val toast = Toast.makeText(this.applicationContext, text, duration)
     toast.show()
+    ToastContainer.toast = toast
     return toast
 }
 
@@ -20,7 +26,9 @@ fun Context.toast(
     resId: Int,
     duration: Int = Toast.LENGTH_SHORT
 ): android.widget.Toast {
+    ToastContainer.toast?.cancel()
     val toast = Toast.makeText(this.applicationContext, resId, duration)
     toast.show()
+    ToastContainer.toast = toast
     return toast
 }
